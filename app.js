f9ac1c61e3ff53ca6a45f8e9c3007c4a05207248
lavaFrai <vladimirkurdiukovv@yandex.ru>
2021-10-19 17:42:18 +0300

    0.1.0

---
+++ app.js
@@ -0,0 +1,25 @@
+setInterval(function(){
+    if (document.querySelector(".shp-copyright") != null)
+        document.querySelector(".shp-copyright").remove();
+
+    if (document.querySelector("#chat-24-widget-container") != null)
+        document.querySelector("#chat-24-widget-container").remove();
+
+    if (document.querySelector(".project-name") != null)
+        document.querySelector(".project-name").innerHTML = "Залупа";
+
+    if (document.querySelector(".brand-logo") != null)
+        document.querySelector(".project-name").innerHTML = "Залупа";
+
+    if (document.querySelector(".title-signout") != null)
+        document.querySelector(".title-signout").innerHTML = "Пойти нахуй";
+
+    if (document.querySelector(".brand-logo") != null)
+        document.querySelector(".project-name").innerHTML = "Залупа";
+
+    if (document.querySelector(".brand-logo") != null)
+        document.querySelector(".brand-logo").style.backgroundImage = "url(\"https://steamuserimages-a.akamaihd.net/ugc/859487187058166664/F1394B31361B706826D9DE26FBA775FAC0666975/?imw=637&imh=358&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=true\")";
+        
+    if (document.querySelector(".shp_notification__img") != null)
+        document.querySelector(".shp_notification__img").src = "https://i.ibb.co/9WJJpBS/Cm-WI4e-Z74u0-removebg-preview.png";
+}, 1000)
